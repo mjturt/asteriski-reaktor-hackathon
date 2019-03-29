8f89f35d2782b9a1c8a12bd16ba1b196403d6c7b
mjturt <maks.turtiainen@gmail.com>
2019-03-30 01:31:34 +0200

    Fresh start

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,10 +1,5 @@
-/**
- * Created by barrett on 8/28/14.
- */
-
 var mysql = require('mysql');
 var dbconfig = require('../config/database');
-
 var connection = mysql.createConnection(dbconfig.connection);
 
 connection.query('\
