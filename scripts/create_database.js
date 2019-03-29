ad324087c3e29c79100975533a195d14d68403f9
mjturt <maks.turtiainen@gmail.com>
2019-03-30 01:12:55 +0200

    Database scheme

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -7,16 +7,32 @@ var dbconfig = require('../config/database');
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query('CREATE DATABASE ' + dbconfig.database);
+connection.query('\
+CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.customers_table + '` ( \
+    `idx` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `email` VARCHAR(20) NOT NULL, \
+    `firstName` VARCHAR(20) NOT NULL, \
+    `password` CHAR(60) NOT NULL, \
+        PRIMARY KEY (`idx`), \
+    `bonusPoints` INT UNSIGNED NOT NULL, \
+    `memberSince` CHAR(60) NOT NULL, \
+    UNIQUE INDEX `idx_UNIQUE` (`idx` ASC), \
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC) \
+)');
 
 connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
+CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.employees_table + '` ( \
+    `idx` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+    `privateEmail` VARCHAR(20) NOT NULL, \
+    `workEmail` VARCHAR(20) NOT NULL, \
+    `icon` VARCHAR(20) NOT NULL, \
+    `firstName` VARCHAR(20) NOT NULL, \
     `password` CHAR(60) NOT NULL, \
-        PRIMARY KEY (`id`), \
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+        PRIMARY KEY (`idx`), \
+    `store` CHAR(60) NOT NULL, \
+    `employeeSince` CHAR(60) NOT NULL, \
+    UNIQUE INDEX `idx_UNIQUE` (`idx` ASC), \
+    UNIQUE INDEX `workEmail_UNIQUE` (`workEmail` ASC) \
 )');
 
 console.log('Success: Database Created!')
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // server.js
 
-// set up ======================================================================
-// get all the tools we need
 var express  = require('express');
 var session  = require('express-session');
 var cookieParser = require('cookie-parser');
@@ -9,7 +7,6 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
 var port     = process.env.PORT || 8080;
-
 var passport = require('passport');
 var flash    = require('connect-flash');
 
@@ -18,24 +15,20 @@ var flash    = require('connect-flash');
 
 require('./config/passport')(passport); // pass passport for configuration
 
-
-
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs'); // set up ejs for templating
-
 // required for passport
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
+    secret: 'vidyapathaisalwaysrunning',
+    resave: true,
+    saveUninitialized: true
+} )); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
